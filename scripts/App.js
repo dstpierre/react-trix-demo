ec8e88e0c714284018d1d886c97f4282ca5c7787
Dominic St-Pierre <dominicstpierre@gmail.com>
2017-04-06 05:20:55 -0400

    simple react-trix demo inside an es6 react app

--- scripts/App.js
+++ scripts/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
+import { TrixEditor } from "react-trix";
+
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      editor: null
+    }
+  }
+  handleChange(html, text) {
+    console.log(html, text);
+  }
+  handleEditorReady(e) {
+    this.setState({ editor: e });
+    console.log("editor ready");
+    console.log(e);
+  }
   render() {
     return (
-      // Add your component markup and other subcomponent references here.
-      <h1>Hello, World!</h1>
+      <div>
+        <h1>Hello, World!</h1>
+        <TrixEditor autoFocus={true} onChange={this.handleChange} value="ok"
+          onEditorReady={this.handleEditorReady.bind(this)} />
+      </div>
     );
   }
 }
